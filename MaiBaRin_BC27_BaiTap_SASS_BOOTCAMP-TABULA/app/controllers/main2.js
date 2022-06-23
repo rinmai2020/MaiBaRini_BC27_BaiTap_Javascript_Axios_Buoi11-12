723e898e452771ae74b67eb607520350165b6f2f
Victor Robert <rinmai2019@gmail.com>
2022-06-23 21:13:06 +0700

    update

--- MaiBaRin_BC27_BaiTap_SASS_BOOTCAMP-TABULA/app/controllers/main2.js
+++ MaiBaRin_BC27_BaiTap_SASS_BOOTCAMP-TABULA/app/controllers/main2.js
@@ -8,22 +8,24 @@ function main() {
     var html = "";
     for (var i = 0; i < users.length; i++) {
       var user = users[i];
-      html += `
-    <div class="col-lg-3 col-md-6 col-sm-6 py-0 py-sm-4 px-sm-4">
-      <div class="card card-style ">
-          <div class="card-img">
-            <img class="card-img-top" src="${user.image}"/>
+      if (user.typeUser === "GV") {
+        html += `
+        <div class="col-lg-3 col-md-6 col-sm-6 py-0 py-sm-4 px-sm-4">
+          <div class="card card-style ">
+              <div class="card-img">
+                <img class="card-img-top" src="${user.image}"/>
+              </div>
+              <div class="card-body">
+                <h6>${user.languages}</h6>
+                <h1>${user.fullName}</h1>
+                <p class="card-text">
+                ${user.description}
+                </p>
+              </div>
           </div>
-          <div class="card-body">
-            <h6>${user.languages}</h6>
-            <h1>${user.fullName}</h1>
-            <p class="card-text">
-            ${user.description}
-            </p>
-          </div>
-      </div>
-    </div>
-    `;
+        </div>
+        `;
+      }
     }
     document.getElementById("productRow").innerHTML = html;
   });
